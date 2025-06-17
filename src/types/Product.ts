e115b50e84d06dd88bb5447d990bbe5192e10694
Nadir Inab <nadir.inab.dev@gmail.com>
2025-06-17 12:30:11 +0100

    folder structure is finished

---
+++ src/types/Product.ts
@@ -0,0 +1,13 @@
+
+export interface product {
+    id: number, 
+    title: string, 
+    price: number, 
+    description: string, 
+    image: string, 
+    category: string, 
+    rating: {
+        rate: number,
+        count: number
+      }
+}
